8a9d1171621530e9d16fa532df115f4c3a08eec0
briannamalpar <98166536+briannamalpar@users.noreply.github.com>
2023-04-08 21:52:19 -0400

    new ver

--- LAB2/main.js
+++ LAB2/main.js
@@ -1,20 +1,46 @@
-async function startVideoCapture() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-          width: { exact: 1280 },
-          height: { exact: 720 },
-          frameRate: { exact: 30 },
-        },
-        audio: false,
-      });
-      const videoElement = document.createElement('video');
-      videoElement.srcObject = stream;
-      videoElement.play();
-      document.body.appendChild(videoElement);
-    } catch (error) {
-      console.error('Error starting video capture:', error);
+
+const videoElement = document.createElement('video');
+
+// prompts user permission to access camera
+navigator.mediaDevices.getUserMedia({ 
+    video: {
+        width: { exact: 1280 },
+        height: { exact: 720 },
+        frameRate: { exact: 30 }
     }
-  }
+})
+  .then((stream) => {
+    videoElement.srcObject = stream;
+    videoElement.play();
+
+    setInterval(() => {
+      //capture video frames
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+      canvas.width = videoElement.videoWidth;
+      canvas.height = videoElement.videoHeight;
+      context.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
+      const videoSegment = canvas.toDataURL('video/webm'); // Convert frames to video segment
   
-  startVideoCapture();
+      // Save video segment to the server using an HTTP request
+      fetch('/saveVideoSegment', {
+        method: 'POST',
+        body: JSON.stringify({ segmentData: videoSegment }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (data.message) {
+          console.log('Video segment saved successfully');
+        } else {
+          console.error('Failed to save video segment:', data.error);
+        }
+      })
+      .catch(error => {
+        console.error('Failed to save video segment:', error);
+      });
+    }, 10000); // Capture video segment every 10 seconds
+  })
+  .catch(error => {
+    console.error('Failed to access camera:', error);
+  });
